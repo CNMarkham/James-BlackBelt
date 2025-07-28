a369af85fd8e9080d616bf490d2a17155cd1a60a
.(period) <markhamonca@codeninjas.com>
2025-07-28 17:59:18 -0400

    35

--- James-blackbelt/Assets/Script/breakable stuff.cs
+++ James-blackbelt/Assets/Script/breakable stuff.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class breakablebox : MonoBehaviour, IDamageable
 {
+
+    public float hits;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hits = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hits >= 5)
+        {
+            Destroy(this.gameObject);
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<Bullet>())
         {
-            Destroy(this.gameObject);
+            hits += 1;
         }
     }
     public void Damage(float damage)
